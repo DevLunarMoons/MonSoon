4f8f2521fb80b5631868b7c27785be37b5a74dd0
LunarMoons <mangobananascommissions@gmail.com>
2024-12-08 19:37:41 -0600

    Update and rename errorTyphoon to errorTyphoon.lua

--- error/errorTyphoon.lua
+++ error/errorTyphoon.lua
@@ -1,7 +1,17 @@
 -- ~:| error |:~ --
+game:GetService("StarterGui"):SetCore("SendNotification",{
+			Title = "Look at your console, buddy...",
+			Text = ">:("
+		})
+task.wait(5)
 print ("Ies a goofy goober jyas :P")
+task.wait(4)
 print ("Yous a goofi goober jya :3")
+task.wait(4)
 print ("Wes alls goofe goobers jya")
+task.wait(4)
+print ("goofy goofi goofe goobers jya!!")
+task.wait(3)
 -- ~:| error |:~ --
 game.Players.LocalPlayer:Kick("\n(ERROR ID: Typhoon) (Code:0041)\n\nYou do not have access to this script.\n")
 -- ~:| error |:~ --
