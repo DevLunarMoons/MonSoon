9cb2575a18fa571113dbc0e7eb0c6a17dbc86e9a
LunarMoons <mangobananascommissions@gmail.com>
2024-12-11 13:43:07 -0600

    Update SenseESP.lua

--- OfficialReleases/SenseESP.lua
+++ OfficialReleases/SenseESP.lua
@@ -4,7 +4,7 @@ local Sense = loadstring(game:HttpGet('https://sirius.menu/sense'))()
 -- 2. Change the configuration.
 Sense.teamSettings.enemy.enabled = true
 Sense.teamSettings.enemy.box = true
-Sense.teamSettings.enemy.boxColor[1] = Color3.new(0, 0.25, 0.75)
+Sense.teamSettings.enemy.boxColor[1] = Color3.new(57, 255, 20)
 
 -- 3. Load the esp. It doesn't really matter where you put this, but it's recommended you put it at the end of your script.
 Sense.Load()
