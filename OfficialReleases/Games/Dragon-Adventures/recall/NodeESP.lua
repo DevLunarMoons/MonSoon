ca168c1cadf44e36f88ede75af81319d23f34276
LunarMoons <mangobananascommissions@gmail.com>
2024-12-12 08:53:25 -0600

    Update NodeESP.lua

--- OfficialReleases/Games/Dragon-Adventures/recall/NodeESP.lua
+++ OfficialReleases/Games/Dragon-Adventures/recall/NodeESP.lua
@@ -1,4 +1,18 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/DevLunarMoons/MonSoon/main/OfficialReleases/SenseESP.lua'))()
+print ("Sense Loaded!")
+-- 1. Load the library
+local Sense = loadstring(game:HttpGet('https://sirius.menu/sense'))()
+
+-- 2. Change the configuration.
+Sense.teamSettings.enemy.enabled = true
+Sense.teamSettings.enemy.box = true
+Sense.teamSettings.enemy.boxColor[1] = Color3.new(57, 255, 20)
+
+-- 3. Load the esp. It doesn't really matter where you put this, but it's recommended you put it at the end of your script.
+Sense.Load()
+
+-- 4. Unload the esp. When you unload Sense, it will clean up every drawing object and instance it has made.
+--Sense.Unload()
+
 print ("Dragon Adventures ESP Loaded")
 --Locals for each node.
 --local EggNode = workspace.Interactions.Nodes.Eggs.ActiveNodes:GetChildren()[#].EggModel.Egg
