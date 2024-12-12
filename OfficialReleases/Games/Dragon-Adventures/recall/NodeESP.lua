4044ef4eb119e6e1cbf4fa4570d999b208410f99
LunarMoons <mangobananascommissions@gmail.com>
2024-12-12 09:30:23 -0600

    Update NodeESP.lua

--- OfficialReleases/Games/Dragon-Adventures/recall/NodeESP.lua
+++ OfficialReleases/Games/Dragon-Adventures/recall/NodeESP.lua
@@ -1,18 +1,21 @@
+
+-------------------------------------------------
+--           Sense | made by: Sirius           --
+--                Instance ESP                 --
+--           Game: Dragon Adventures           --
+-------------------------------------------------
+
 print ("Sense Loaded!")
--- 1. Load the library
+-- This is required at the top of the script.
 local Sense = loadstring(game:HttpGet('https://sirius.menu/sense'))()
+    Sense.teamSettings.enemy.enabled = false
+    Sense.teamSettings.enemy.box = false
+    Sense.teamSettings.enemy.boxColor[1] = Color3.new(57, 255, 20)
 
--- 2. Change the configuration.
-Sense.teamSettings.enemy.enabled = true
-Sense.teamSettings.enemy.box = false
-Sense.teamSettings.enemy.boxColor[1] = Color3.new(57, 255, 20)
-
--- 3. Load the esp. It doesn't really matter where you put this, but it's recommended you put it at the end of your script.
 Sense.Load()
-
--- 4. Unload the esp. When you unload Sense, it will clean up every drawing object and instance it has made.
---Sense.Unload()
-
+-- Sense.Unload()
+-- Use as the command to unload ESP
+----------------------------------------------------------------------------------------------
 print ("Dragon Adventures ESP Loaded")
 --Locals for each node.
 --local EggNode = workspace.Interactions.Nodes.Eggs.ActiveNodes:GetChildren()[#].EggModel.Egg
@@ -22,11 +25,19 @@ print ("Dragon Adventures ESP Loaded")
 --local ChestNode = workspace.Interactions.Nodes.Treasure[#].Body
 --local MobNode = workspace.Interactions.Nodes.Mobs:GetChildren()[#]
 --local MobsNodes = workspace.MobFolder:GetChildren()
+----------------------------------------------------------------------------------------------
+-- Individual ESP for each object. Do not use :GetChildren(), rather use :GetDescendants().
 
+-- TODO: Egg ESP needs to be updated when a child is added to the "ActiveNodes" folder.
+-- Egg ESP
 local eggESP = {}
 local eggName = "Egg"
-
 for _, resource in workspace.Interactions.Nodes.Eggs.ActiveNodes:GetDescendants() do
+--    for i,v in workspace:GetDescendants() do
+--	    v.DescendantAdded:Connect(function(child)
+--		    print(child)
+--	    end)
+--    end
     if resource.Name ~= eggName then 
         continue
     end
@@ -45,5 +56,26 @@ for _, resource in workspace.Interactions.Nodes.Eggs.ActiveNodes:GetDescendants(
         })
     )
 end
--- workspace.Interactions.Nodes.Eggs.ActiveNodes.Part
--- workspace.Interactions.Nodes.Eggs.ActiveNodes:GetChildren()
+
+-- Food ESP
+local foodESP = {}
+local foodName = "BillboardPart"
+for _, resource in workspace.Interactions.Nodes.Food:GetDescendants() do
+    if resource.Name ~= foodName then 
+        continue
+    end
+    table.insert(
+        foodESP,
+        Sense.AddInstance(resource, {
+            enabled = true,
+            text = "Food ({distance} studs)", -- Placeholders: {name}, {distance}, {position}
+            textColor = { Color3.new(1,1,1), 1 },
+            textOutline = true,
+            textOutlineColor = Color3.new(),
+            textSize = 13,
+            textFont = 2,
+            limitDistance = false,
+            maxDistance = 150
+        })
+    )
+end
