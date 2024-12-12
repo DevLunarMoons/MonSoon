584e1fd3a8862991ae7667d85d6be95fcbce374a
LunarMoons <mangobananascommissions@gmail.com>
2024-12-12 08:33:54 -0600

    Update NodeESP.lua

--- OfficialReleases/Games/Dragon-Adventures/recall/NodeESP.lua
+++ OfficialReleases/Games/Dragon-Adventures/recall/NodeESP.lua
@@ -1,18 +1,3 @@
-print ("Sense Loaded!")
--- 1. Load the library
-local Sense = loadstring(game:HttpGet('https://sirius.menu/sense'))()
-
--- 2. Change the configuration.
-Sense.teamSettings.enemy.enabled = true
-Sense.teamSettings.enemy.box = true
-Sense.teamSettings.enemy.boxColor[1] = Color3.new(57, 255, 20)
-
--- 3. Load the esp. It doesn't really matter where you put this, but it's recommended you put it at the end of your script.
-Sense.Load()
-
--- 4. Unload the esp. When you unload Sense, it will clean up every drawing object and instance it has made.
---Sense.Unload()
-
 print ("Dragon Adventures ESP Loaded")
 --Locals for each node.
 --local EggNode = workspace.Interactions.Nodes.Eggs.ActivesNodes:GetChildren()[#].EggModel.Egg
@@ -23,21 +8,25 @@ print ("Dragon Adventures ESP Loaded")
 --local MobNode = workspace.Interactions.Nodes.Mobs:GetChildren()[#]
 --local MobsNodes = workspace.MobFolder:GetChildren()
 
---Use for Resource and Egg nodes.
+local eggESP = {}
+local eggName = "Egg"
 
-local object = Sense.AddInstance(workspace.Interactions.Nodes.Eggs.ActivesNodes:GetChildren(), {
-    --enabled = false,
-    text = "{name}", -- Placeholders: {name}, {distance}, {position}
-    textColor = { Color3.new(1,1,1), 1 },
-    textOutline = true,
-    textOutlineColor = Color3.new(),
-    textSize = 13,
-    textFont = 2,
-    limitDistance = false,
-    maxDistance = 150
+for _, resource in workspace.Interactions.Nodes.Eggs.ActivesNodes:GetDescendants() do
+    if resource.Name -= eggName then 
+        continue
+    end
+    table.insert(
+        eggESP,
+        Sense.AddInstance(resource, {
+        enabled = false,
+        text = "{name} ({distance} studs)", -- Placeholders: {name}, {distance}, {position}
+        textColor = { Color3.new(1,1,1), 1 },
+        textOutline = true,
+        textOutlineColor = Color3.new(),
+        textSize = 13,
+        textFont = 2,
+        limitDistance = false,
+        maxDistance = 150
 })
-
-object.options.enabled = true
-
 -- workspace.Interactions.Nodes.Eggs.ActiveNodes.Part
 -- workspace.Interactions.Nodes.Eggs.ActiveNodes:GetChildren()
